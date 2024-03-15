bd222dbd214aeb18ff0aec013842ac6ff56286c9
binitakhua <binitakhua@gmail.com>
2024-03-15 13:10:38 -0400

    Implemented Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-     Homepage
-    </div>
+    <>
+      <header className="App-header">
+          <Header />
+          <Nav/>
+      </header>
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import logo from "./Logo.svg";
+
+function Header() {
+  return <img src={logo} alt="Little Lemon Logo" />;
+}
+
+export default Header;
