c6132cf17d5c0b787da00b41f9b123aae642296e
binitakhua <binitakhua@gmail.com>
2024-03-15 13:16:05 -0400

    Added OGP to the web app

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ import Footer from './Footer';
 function App() {
   return (
     <>
+    <Helmet>
+        <title>Little Lemon - Fresh and Zesty Eats</title>
+        <meta name="description" content="Little Lemon is your destination for fresh and zesty eats. Enjoy our delicious meals and refreshing drinks." />
+        <meta property="og:title" content="Little Lemon - Fresh and Zesty Eats" />
+        <meta property="og:description" content="Little Lemon is your destination for fresh and zesty eats. Enjoy our delicious meals and refreshing drinks." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://www.littlelemon.com" />
+        <meta property="og:image" content="http://www.littlelemon.com/logo.png" />
+      </Helmet>
       <header className="App-header">
           <Header />
           <Nav/>
